5c1f9d90b00f301dba2a37806b789c40036c20c0
Harpo Harbert <ryan_harbert@wgbh.org>
2024-09-06 10:40:59 -0700

    Refactors cert script for functions, adds usage help

--- elasticsearch/elastic-certs.sh
+++ elasticsearch/elastic-certs.sh
@@ -1,37 +1,50 @@
 #!/bin/bash
 
-# This script will update the elastic-certs secret with the latest certificate from Traefik
-
-DOMAIN=elastic.wgbh-mla.org
-
-if [ -z "$DOMAIN" ]; then
-    echo "Please provide a domain"
-    echo "Usage: $0 <domain>"
+help() {
+    echo
+    echo "Usage: $0 <update-elasticsearch|update-kibana>"
+    echo
+    echo This script gets the latest certificate from Traefik
+    echo and updates the Elasticsearch or Kibana k8s secret
+    echo
     exit 1
-fi
+}
 
->&2 echo "DOMAIN: $DOMAIN"
+get-traefik() {
+    # Get the name of the running Traefik pod
+    TRAEFIK=$(kubectl -n traefik -o json get po | jq -r .items[0].metadata.name)
 
-# Get the name of the running Traefik pod
-TRAEFIK=$(kubectl -n traefik -o json get po | jq -r .items[0].metadata.name)
+    if [ -z "$TRAEFIK" ]; then
+        echo "Traefik pod not found"
+        exit 1
+    fi
+    echo "$TRAEFIK"
+}
 
-if [ -z "$TRAEFIK" ]; then
-    echo "Traefik pod not found"
-    exit 1
-fi
+get-cert() {
+    # Get the certificate object from Traefik that matches the domain
+    DOMAIN=$1
+    CERT=$(kubectl exec -n traefik $TRAEFIK -- cat /ssl-certs/acme-production.json | jq --arg domain $DOMAIN -r '.production.Certificates[] | select( .domain.main==$domain ).certificate')
 
->&2 echo "TRAEFIK: $TRAEFIK"
+    if [ -z "$CERT" ]; then
+        echo "Certificate not found for $DOMAIN"
+        exit 1
+    fi
+    echo "$CERT"
+}
 
-# Get the certificate object from Traefik that matches the domain
-CERT=$(kubectl exec -n traefik $TRAEFIK -- cat /ssl-certs/acme-production.json | jq --arg domain $DOMAIN -r '.production.Certificates[] | select( .domain.main==$domain ).certificate' )
+update-elasticsearch() {
+    # Update the Elasticsearch secret with the latest certificate
+    TRAEFIK=$(get-traefik)
+    CERT=$(get-cert "elastic.wgbh-mla.org")
+    kubectl -n elastic patch secret elastic-certs -p "{\"data\":{\"tls.crt\":\"$CERT\"}}"
+}
 
-if [ -z "$CERT" ]; then
-    echo "Certificate not found"
-    exit 1
-fi
+update-kibana() {
+    # Update the Kibana secret with the latest certificate
+    TRAEFIK=$(get-traefik)
+    CERT=$(get-cert "search.wgbh-mla.org")
+    kubectl -n elastic patch secret kibana-certs -p "{\"data\":{\"tls.crt\":\"$CERT\"}}"
+}
 
-# echo "$CERT"
-
-# Modify the k8s secret
-# When modifying the kibana secret, be sure to modify the `kibana-certs` secret
-kubectl -n elastic patch secret elastic-certs -p "{\"data\":{\"tls.crt\":\"$CERT\"}}"
+"$@" || help
